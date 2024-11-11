3bc517c0e795ed61f24a0349978a88f0baa59883
minmmmmin <kotomi.hinahima@gmail.com>
2024-11-11 09:19:08 +0900

    第7回

---
+++ .netlify/functions-serve/hello/hello.mjs
@@ -0,0 +1,20 @@
+
+import {createRequire as ___nfyCreateRequire} from "module";
+import {fileURLToPath as ___nfyFileURLToPath} from "url";
+import {dirname as ___nfyPathDirname} from "path";
+let __filename=___nfyFileURLToPath(import.meta.url);
+let __dirname=___nfyPathDirname(___nfyFileURLToPath(import.meta.url));
+let require=___nfyCreateRequire(import.meta.url);
+
+
+// netlify/functions/hello.mjs
+var hello_default = async () => {
+  const data = {
+    message: "Hello, World!"
+  };
+  return new Response(JSON.stringify(data));
+};
+export {
+  hello_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsibmV0bGlmeS9mdW5jdGlvbnMvaGVsbG8ubWpzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJleHBvcnQgZGVmYXVsdCBhc3luYyAoKSA9PiB7XHJcbiAgICBjb25zdCBkYXRhID0ge1xyXG4gICAgICBtZXNzYWdlOiBcIkhlbGxvLCBXb3JsZCFcIixcclxuICAgIH07XHJcbiAgICByZXR1cm4gbmV3IFJlc3BvbnNlKEpTT04uc3RyaW5naWZ5KGRhdGEpKTtcclxuICB9OyJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7Ozs7QUFBQSxJQUFPLGdCQUFRLFlBQVk7QUFDdkIsUUFBTSxPQUFPO0FBQUEsSUFDWCxTQUFTO0FBQUEsRUFDWDtBQUNBLFNBQU8sSUFBSSxTQUFTLEtBQUssVUFBVSxJQUFJLENBQUM7QUFDMUM7IiwKICAibmFtZXMiOiBbXQp9Cg==
